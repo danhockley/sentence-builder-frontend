d8bbd2eeb132815dd30e004c46a8e2f6b9df4bd0
Daniel Hockley <daniel@tforge.co.za>
2023-11-20 12:16:31 +0200

    Initial - router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { SentenceDisplayComponent } from './components/sentence-display/sentence-display.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'sentence-builder', component: SentenceDisplayComponent },
+    { path: '', redirectTo: '/sentence-builder', pathMatch: 'full' },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
