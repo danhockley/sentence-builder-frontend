2b568fe8e8a0c7cb02c16eacb313641256c76579
Daniel Hockley <daniel@tforge.co.za>
2023-11-20 12:26:14 +0200

    Initial - API service setup

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ApiService {
+    private apiUrl = 'https://api-gateway-url' // TODO: Replace with actual API Gateway URL
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    getWordsByType(wordType: string): Observable<string[]> {
+        return this.http.get<string[]>(`${this.apiUrl}/words/${wordType}`)
+    }
+
+    submitSentence(sentence: string): Observable<any> {
+        return this.http.post(`${this.apiUrl}/sentences`, { content: sentence })
+    }
+
+    getAllSentences(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.apiUrl}/sentences`)
+    }
 }
